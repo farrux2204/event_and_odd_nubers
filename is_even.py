b2835556b13a9d8dd1f7bc23e4e6eeb3d4560a31
Farrux <ulugbekovfarrux0822@gmail.com>
2022-10-06 09:53:15 +0500

    1-bajardim

--- is_even.py
+++ is_even.py
@@ -1,5 +1,6 @@
 #Create a variable "number" and assign it a integer value.
 
 #Print 1 if the number is even, otherwise print 0.
-number=5
-a=(number+1)%2
+number=11
+number=(number+1)%2
+print(number)
