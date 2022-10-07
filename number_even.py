b4e777325b6f037d2de633f6f2290c82a49d532d
Farrux <ulugbekovfarrux0822@gmail.com>
2022-10-07 09:19:17 +0500

    4-bajardim

--- number_even.py
+++ number_even.py
@@ -4,7 +4,7 @@
 
 #Print the number of even digits in the variable "var_int".
 
-a=5686
+a=1233
 var_int=0
 
 a1 = a%10
@@ -12,15 +12,19 @@ var_int=var_int+(a1+1)%2
 a//=10 
 
 
+
 a2 = a%10
 var_int=var_int+(a2+1)%2
 a//=10
 
-a3 = a%10
-var_int=var_int+(a3+1)%2
-a//=10
 
 a3 = a%10
 var_int=var_int+(a3+1)%2
 a//=10
+
+
+a4 = a%10
+var_int=var_int+(a4+1)%2
+a//=10
+
 print(var_int)
