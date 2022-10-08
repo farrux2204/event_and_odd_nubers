0cf439840641b952243a4f559b66f6c1eae45292
Farrux <ulugbekovfarrux0822@gmail.com>
2022-10-08 11:50:01 +0500

    1

--- is_odd.py
+++ is_odd.py
@@ -1,6 +1,6 @@
 #Create a variable "number" and assign it a integer value.
 
 #Print 1 if the number is odd, otherwise print 0.
-number=9
-a=(number+1)%3
-print(a)
+number=101
+a=(number)%2
+print(a) 
